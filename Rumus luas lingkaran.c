db522bd753495ce86e64827b1a97a948aa29851d
Zulfikar Hidayatullah <45659177+VixiaZ@users.noreply.github.com>
2019-01-03 16:56:58 +0700

    Add files via upload

---
+++ Rumus luas lingkaran.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+const float phi = 3.14;
+const char nama[] = "vixia";
+
+void main () {
+    printf("%f\n",phi);
+    float luas = 2 * 2 * phi;
+    printf("%f\n",luas);
+    printf("%s\n",nama);
+}
